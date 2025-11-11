9e03c46fca45eebf7d25bf502bdd2ee4d13145f4
pv <petasvestartas@gmail.com>
2025-11-11 10:57:34 +0100

    INIT

---
+++ src/compas_tf/__init__.py
@@ -0,0 +1,5 @@
+__author__ = ["Petras Vestartas"]
+__copyright__ = "Petras Vestartas, Tom van Mele"
+__license__ = "MIT License"
+__email__ = "petrasvestartas@gmail.com"
+__version__ = "0.1.0"
---
+++ tasks.py
@@ -0,0 +1,33 @@
+import os
+
+from compas_invocations2 import build
+from compas_invocations2 import docs
+from compas_invocations2 import style
+from compas_invocations2 import tests
+from invoke.collection import Collection
+
+ns = Collection(
+    docs.help,
+    style.check,
+    style.lint,
+    style.format,
+    docs.docs,
+    docs.linkcheck,
+    tests.test,
+    tests.testdocs,
+    tests.testcodeblocks,
+    build.prepare_changelog,
+    build.clean,
+    build.release,
+    build.build_ghuser_components,
+)
+ns.configure(
+    {
+        "base_folder": os.path.dirname(__file__),
+        "ghuser": {
+            "source_dir": "src/compas_tf_ghpython/components",
+            "target_dir": "src/compas_tf_ghpython/components/ghuser",
+            "prefix": "compas_tf: ",
+        },
+    }
+)
