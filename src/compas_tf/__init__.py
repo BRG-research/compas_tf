063cb12e12e1a9bf589af27fa5de6f01c5411ac1
petrasvestartas <petrasvestartas@gmail.com>
2026-08-09 20:59:28 +0200

    Bump version to 0.1.2

--- src/compas_tf/__init__.py
+++ src/compas_tf/__init__.py
@@ -2,7 +2,7 @@ __author__ = ["Petras Vestartas"]
 __copyright__ = "Petras Vestartas, Tom van Mele"
 __license__ = "MIT License"
 __email__ = "petrasvestartas@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from compas_tf.geometry import (
     PolylineOffset,
