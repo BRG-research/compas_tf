57030e01ff20943c26618894450cc2e30953c3a4
petrasvestartas <petrasvestartas@gmail.com>
2026-08-10 15:38:55 +0200

    Bump version to 0.1.10

--- src/compas_tf/__init__.py
+++ src/compas_tf/__init__.py
@@ -2,7 +2,7 @@ __author__ = ["Petras Vestartas"]
 __copyright__ = "Petras Vestartas, Tom van Mele"
 __license__ = "MIT License"
 __email__ = "petrasvestartas@gmail.com"
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 # Collected by compas' plugin system: it imports every installed top-level
 # package named compas*, then imports the modules listed here and registers the
